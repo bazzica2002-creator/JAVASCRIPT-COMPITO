e8fd1f5fbaa2b7e6a4d58ec7c81347d3e4d60979
bazzica2002-creator <bazzica2002@gmail.com>
2025-09-29 15:22:50 +0200

    1 esercizio

---
+++ D1.js
@@ -0,0 +1,64 @@
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let stringa 
+stringa = 'Stringa Sequenza di caratteri alfanumerici. Ma non va mai messo un numero prima di una lettera ma sempre dopo'
+let number
+number = 'Number Questo tipo di dato è numerico. Può essere integrale o decimale'
+let boolean 
+boolean = 'Boolean Può assumere solo i valori true o false'
+let nul 
+nul = 'nul Lo rilevo nelle variabili ma a cui non ho assegnato a nessn valore'
+let undrfined
+undrfined = 'Undrfined Questo data type lo rilevo quando qualcosa non è stato ben definito in una variabile a cui ho assegnato un valore'
+
+console.log(stringa)
+console.log(number)
+console.log(boolean)
+console.log(nul)
+console.log(undrfined)
+
+
+
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
